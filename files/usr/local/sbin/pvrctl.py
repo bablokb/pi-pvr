cdaad16ff09effefa1f76ecf46d34299cbbb8ac8
Bernhard Bablok <bablokb@gmx.de>
2018-12-26 19:22:32 +0100

    initial commit

---
+++ files/usr/local/sbin/pvrctl.py
@@ -0,0 +1,170 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# --------------------------------------------------------------------------
+# Control-script for tvheadend-based pvr
+#
+# Main program.
+#
+# Author: Bernhard Bablok
+# License: GPL3
+#
+# Website: https://github.com/bablokb/pi-pvr
+#
+# --------------------------------------------------------------------------
+
+TVHEADEND_UPCOMING_API="api/dvr/entry/grid_upcoming"
+
+# --- system imports   -----------------------------------------------------
+
+from argparse import ArgumentParser
+from operator import itemgetter
+import sys, os, datetime, pprint
+import requests
+import configparser
+
+# --- private imports   -----------------------------------------------------
+
+from pvrctl_msg      import Msg as Msg
+
+# --- helper-class for options   --------------------------------------------
+
+class Options(object):
+  pass
+
+# --- query upcoming events   -----------------------------------------------
+
+def api_upcoming():
+  """ query upcoming events """
+
+  url = "http://%s:%s@%s:9981/%s" % (
+                                    options.config['user'],
+                                    options.config['password'],
+                                    options.config['hostname'],
+                                    TVHEADEND_UPCOMING_API)
+  req = requests.get(url)
+  return sorted(req.json()['entries'], key=itemgetter('start_real')) 
+
+
+# --- print upcoming recordings   -------------------------------------------
+
+def print_upcoming(options,all=True):
+  """ output upcoming recordings """
+
+  next_recordings = api_upcoming()
+
+  for rec in next_recordings:
+    start = datetime.datetime.fromtimestamp(rec['start_real'])
+    end   = datetime.datetime.fromtimestamp(rec['stop_real'])
+    print("""
+channel:  %s
+title:    %s
+status:   %s
+start:    %s
+end:      %s\n""" % (rec['channelname'],
+                     rec['disp_title'],
+                     rec['status'],
+                     start.strftime("%c"),
+                     end.strftime("%c")))
+    if not all:
+      return    # after printing first entry
+
+# --- output next recording time   ------------------------------------------
+
+def print_next_rec_time(options,out=True):
+  """ output next recording time """
+
+  next_recordings = api_upcoming()
+  start = datetime.datetime.fromtimestamp(next_recordings[0]['start_real'])
+  print(start.strftime("%Y-%m-%d %H:%M:%S"))
+
+# --- process boot logic   --------------------------------------------------
+
+def process_boot_logic(options):
+  """ process boot logic """
+  pass
+
+# --- process post recording logic   ----------------------------------------
+
+def process_post_rec_logic(options):
+  """ process post recording logic """
+  pass
+
+# --- commandline parser  ---------------------------------------------------
+
+def get_parser():
+  parser = ArgumentParser(add_help=False,
+    description='manage boot and shutdown of pvr')
+  
+  parser.add_argument('-u', '--upcoming-recordings', action='store_true',
+    dest='do_upcoming',
+    help='print start-time of next recording')
+  parser.add_argument('-n', '--next-rec-time', action='store_true',
+    dest='do_next',
+    help='print start-time of next recording')
+  parser.add_argument('-b', '--boot', action='store_true',
+    dest='do_boot',
+    help='execute boot-hook logic')
+  parser.add_argument('-p', '--post-rec', action='store_true',
+    dest='do_post',
+    help='execute post-recording logic')
+
+  parser.add_argument('-q', '--quiet', default=False, action='store_true',
+    dest='quiet',
+    help='do not print anything')
+  parser.add_argument('-l', '--level', metavar='Log-Level',
+    dest='level', default=None,
+    help='print messages at least of given level')
+  parser.add_argument('-h', '--help', action='help',
+    help='print this help')
+
+  parser.add_argument('arguments', nargs='*', metavar='program-arguments',
+    help='additional program arguments')
+  return parser
+
+# --- create configuration object   -----------------------------------------
+
+def get_config(parser):
+  return {
+    "MSG_LEVEL":           parser.get('CONFIG',"MSG_LEVEL"),
+    "NO_SHUTDOWN_BEFORE":  parser.getint('CONFIG',"NO_SHUTDOWN_BEFORE"),
+
+    "hostname":            parser.get('SERVER',"hostname"),
+    "user":                parser.get('SERVER',"user"),
+    "password"  :          parser.get('SERVER',"password")
+    }
+
+# --- main proram   ---------------------------------------------------------
+
+if __name__ == '__main__':
+
+  config_parser = configparser.RawConfigParser()
+  config_parser.read('/etc/pvrctl.conf')
+  try:
+    config = get_config(config_parser)
+  except Exception as e:
+    print("configuration error!")
+    print("ERROR: %s" % e.message)
+    sys.exit(3)
+
+  opt_parser = get_parser()
+  options = opt_parser.parse_args(namespace=Options)
+
+  # configure Msg-class
+  if options.level:
+    Msg.level = options.level
+  else:
+    Msg.level = config["MSG_LEVEL"]
+
+  # add global objects
+  options.config = config
+
+  if options.do_next:
+    print_next_rec_time(options)
+  elif options.do_upcoming:
+    print_upcoming(options)
+  elif options.do_boot:
+    process_boot_logic(options)
+  elif options.do_post:
+    process_post_rec_logic(options)
+  sys.exit(0)
+
---
+++ files/usr/local/sbin/pvrctl_msg.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# --------------------------------------------------------------------------
+# Control-script for tvheadend-based pvr
+#
+# Definition of class Msg
+#
+# Author: Bernhard Bablok
+# License: GPL3
+#
+# Website: https://github.com/bablokb/pi-pvr
+#
+# --------------------------------------------------------------------------
+
+import datetime, sys
+
+class Msg(object):
+  """simple class for messages"""
+
+  MSG_LEVELS={
+    "TRACE":0,
+    "DEBUG":1,
+    "INFO":2,
+    "WARN":3,
+    "ERROR":4
+    }
+
+  level = "INFO"    # override during initialization
+
+  # --- print a message   ---------------------------------------------------
+  
+  def msg(msg_level,text,nl=True):
+    """print a message"""
+    if Msg.MSG_LEVELS[msg_level] >= Msg.MSG_LEVELS[Msg.level]:
+      if nl:
+        now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        sys.stderr.write("[" + msg_level + "] " + "[" + now + "] " + text + "\n")
+      else:
+        sys.stderr.write(text)
+        sys.stderr.flush()
+
+
