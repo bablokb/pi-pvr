094aabe3d12a5449c74e19d8e1a247c6bbeabfa4
Bernhard Bablok <bablokb@gmx.de>
2018-12-30 16:05:29 +0100

    initial commit

---
+++ files/usr/local/sbin/pvr_after_recording.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# --------------------------------------------------------------------------
+# Helper script executed on after a recording from tvheadend
+#
+# Author: Bernhard Bablok
+# License: GPL3
+#
+# Website: https://github.com/bablokb/pi-pvr
+#
+# --------------------------------------------------------------------------
+
+delta_rec="01:00"    # one hour
+
+# shutdown the system after a recording unless
+#   - the system was manually started
+#   - the next recording is within delta_rec
+
+if [ $(cat /var/run/wake-on-rtc.status) = "normal" ]; then
+  return
+fi
+
+if ! pvrctl.py -q -N "$delta_rec"; then
+  # no recording within delta_rec, so shutdown
+  sudo shutdown -h now
+fi
