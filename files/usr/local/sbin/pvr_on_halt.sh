c3f89d0aae175d72acc3476f459f55c8aaa5c917
Bernhard Bablok <bablokb@gmx.de>
2019-01-03 20:07:25 +0100

    removed path from pvrctl.py

--- files/usr/local/sbin/pvr_on_halt.sh
+++ files/usr/local/sbin/pvr_on_halt.sh
@@ -12,8 +12,8 @@
 no_rec_limit="36:00"    # check for recordings within this time
 idle_boot_at="06:00"    # next boot, if no recording is scheduled
 
-if files/usr/local/sbin/pvrctl.py -qN "$no_rec_limit"; then
-  files/usr/local/sbin/pvrctl.py -n
+if pvrctl.py -qN "$no_rec_limit"; then
+  pvrctl.py -n
 else
   date -d "tomorrow $idle_boot_at" +"%Y-%m-%d %H:%M:%S"
 fi
