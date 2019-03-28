8be84d4c7d093994a74ff30855158cf70e866d4e
Bernhard Bablok <bablokb@gmx.de>
2019-03-28 06:34:50 +0100

    set debug-level to debug

--- files/usr/local/sbin/pvrgui.py
+++ files/usr/local/sbin/pvrgui.py
@@ -215,7 +215,7 @@ class PvrGui(fbgui.App):
 if __name__ == '__main__':
 
   config               = fbgui.Settings()
-  config.msg_level     = "TRACE"
+  config.msg_level     = "DEBUG"
   config.bg_color      = BG_COLOR
   config.font_name     = "FreeSans"
   config.font_size     = FONT_LARGE
