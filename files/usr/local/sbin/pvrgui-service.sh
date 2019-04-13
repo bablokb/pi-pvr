43a08b275bced195f39cf10760e8600b28b1e935
Bernhard Bablok <bablokb@gmx.de>
2019-04-13 15:37:07 +0200

    add log-messages

--- files/usr/local/sbin/pvrgui-service.sh
+++ files/usr/local/sbin/pvrgui-service.sh
@@ -13,13 +13,18 @@
 # --------------------------------------------------------------------------
 
 while true; do
-  # start pvrgui.py in the background and wait for it's termination
+  # start pvrgui.py and wait for it's termination
+  logger -t "pvrgui" "starting pvrgui"
   /usr/local/sbin/pvrgui.py
   if [ $? -eq 3 ]; then
     # quit the service and shutdown in case pvrgui.py returns 3
+    logger -t "pvrgui" "pvrgui ended with rc=3. Exiting ..."
     break
   fi
+  logger -t "pvrgui" "starting kodi"
   /usr/bin/kodi
+  logger -t "pvrgui" "kodi ended with rc=$?"
 done
 
+logger -t "pvrgui" "starting shutdown"
 /sbin/shutdown now
